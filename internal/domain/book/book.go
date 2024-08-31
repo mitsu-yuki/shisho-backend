36255fde9dd6380e72e2bec93ab6dd87779bc342
Mitsu <tech.mitsu.yuki@gmail.com>
2024-09-01 00:29:31 +0900

    Add book domain object

---
+++ internal/domain/book/book.go
@@ -0,0 +1,106 @@
+package book
+
+import (
+	"time"
+
+	errDomain "github.com/mitsu-yuki/shisho-backend/internal/domain/error"
+	"github.com/mitsu-yuki/shisho-backend/pkg/ulid"
+)
+
+type Book struct {
+	id           string
+	isbn         string
+	labelID      string
+	publishID    string
+	title        string
+	authorListID string
+	releaseDay   time.Time
+	price        int
+	explain      string
+}
+
+func newBook(
+	id string,
+	isbn string,
+	labelID string,
+	publishID string,
+	title string,
+	authorListID string,
+	releaseDay time.Time,
+	price int,
+	explain string,
+) (*Book, error) {
+	// レーベルIDのバリデーション
+	if !ulid.IsValid(labelID) {
+		return nil, errDomain.NewError("labelID is invalid.")
+	}
+
+	// 出版社IDのバリデーション
+	if !ulid.IsValid(publishID) {
+		return nil, errDomain.NewError("publishID is invalid.")
+	}
+
+	// 著者リストIDのバリデーション
+	if !ulid.IsValid(authorListID) {
+		return nil, errDomain.NewError("authorListID is invalid")
+	}
+
+	return &Book{
+		id:           id,
+		isbn:         isbn,
+		labelID:      labelID,
+		publishID:    publishID,
+		title:        title,
+		authorListID: authorListID,
+		releaseDay:   releaseDay,
+		price:        price,
+		explain:      explain,
+	}, nil
+}
+
+func Reconstruct(
+	id string,
+	isbn string,
+	labelID string,
+	publishID string,
+	title string,
+	authorListID string,
+	releaseDay time.Time,
+	price int,
+	explain string,
+) (*Book, error) {
+	return newBook(
+		id,
+		isbn,
+		labelID,
+		publishID,
+		title,
+		authorListID,
+		releaseDay,
+		price,
+		explain,
+	)
+}
+
+func NewBook(
+	isbn string,
+	labelID string,
+	publishID string,
+	title string,
+	authorListID string,
+	releaseDay time.Time,
+	price int,
+	explain string,
+) (*Book, error) {
+	return newBook(
+		ulid.NewULID(),
+		isbn,
+		labelID,
+		publishID,
+		title,
+		authorListID,
+		releaseDay,
+		price,
+		explain,
+	)
+}
