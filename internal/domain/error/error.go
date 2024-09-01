1c877422ec9fb63a2835f4fab8d060efb68f8ebb
Mitsu <tech.mitsu.yuki@gmail.com>
2024-09-01 10:56:29 +0900

    Add error domain

---
+++ internal/domain/error/error.go
@@ -0,0 +1,15 @@
+package error
+
+type Error struct {
+	description string
+}
+
+func (e *Error) Error() string {
+	return e.description
+}
+
+func NewError(s string) *Error {
+	return &Error{
+		description: s,
+	}
+}
