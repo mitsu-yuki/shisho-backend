6d35e36cdb70f6add5196278c1e9440a75a33718
Mitsu <tech.mitsu.yuki@gmail.com>
2024-09-02 23:07:50 +0900

    Add validate and test case

--- internal/domain/publish/publish.go
+++ internal/domain/publish/publish.go
@@ -5,6 +5,7 @@ import (
 	"unicode/utf8"
 
 	errDomain "github.com/mitsu-yuki/shisho-backend/internal/domain/error"
+	"github.com/mitsu-yuki/shisho-backend/pkg/text"
 	"github.com/mitsu-yuki/shisho-backend/pkg/ulid"
 )
 
@@ -14,10 +15,10 @@ const (
 )
 
 type Publish struct {
-	id             string
-	name           string
-	namePhonic     string
-	createAt        time.Time
+	id           string
+	name         string
+	namePhonic   string
+	createAt     time.Time
 	lastUpdateAt time.Time
 }
 
@@ -34,15 +35,20 @@ func newPublish(
 	}
 
 	// 名前(読み)のバリデーション
-	if utf8.RuneCountInString(namePhonic) < namePhonicLengthMin {
+	if utf8.RuneCountInString(namePhonic) < namePhonicLengthMin || !text.IsKatakana(namePhonic) {
 		return nil, errDomain.NewError("name is invalid")
 	}
 
+	// 日付のバリデーション(lastUpdateAtのほうが後か)
+	if createAt.After(lastUpdateAt) {
+		return nil, errDomain.NewError("createAt and lastUpdateAt are invalid")
+	}
+
 	return &Publish{
-		id:             id,
-		name:           name,
-		namePhonic:     namePhonic,
-		createAt:        createAt,
+		id:           id,
+		name:         name,
+		namePhonic:   namePhonic,
+		createAt:     createAt,
 		lastUpdateAt: lastUpdateAt,
 	}, nil
 }
--- internal/domain/publish/publish_test.go
+++ internal/domain/publish/publish_test.go
@@ -38,6 +38,50 @@ func TestNewPublish(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "異常系: nameが不正",
+			args: args{
+				name: "",
+				namePhonic: "テスト",
+				createAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+				lastUpdateAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+			},
+			want: nil,
+			wantErr: true,
+		},
+		{
+			name: "異常系: namePhonicが不正",
+			args: args{
+				name: "test",
+				namePhonic: "てすと",
+				createAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+				lastUpdateAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+			},
+			want: nil,
+			wantErr: true,
+		},
+		{
+			name: "異常系: namePhonicが空",
+			args: args{
+				name: "test",
+				namePhonic: "",
+				createAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+				lastUpdateAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+			},
+			want: nil,
+			wantErr: true,
+		},
+		{
+			name: "異常系: 日付が不正",
+			args: args{
+				name: "test",
+				namePhonic: "テスト",
+				createAt: time.Date(2020, 1, 1, 0, 0, 0, 1, time.Local),
+				lastUpdateAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+			},
+			want: nil,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
