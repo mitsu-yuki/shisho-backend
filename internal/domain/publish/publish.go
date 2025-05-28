baac63bf49d8245f5d35b6d6745da314201f922c
Mitsu <tech.mitsu.yuki@gmail.com>
2025-05-28 22:41:11 +0900

    feat: update publish domain

--- internal/domain/publish/publish.go
+++ internal/domain/publish/publish.go
@@ -1,6 +1,7 @@
 package publish
 
 import (
+	"fmt"
 	"time"
 	"unicode/utf8"
 
@@ -20,6 +21,7 @@ type Publish struct {
 	namePhonic   string
 	createAt     time.Time
 	lastUpdateAt time.Time
+	deletedAt    *time.Time
 }
 
 func newPublish(
@@ -28,28 +30,38 @@ func newPublish(
 	namePhonic string,
 	createAt time.Time,
 	lastUpdateAt time.Time,
+	deletedAt *time.Time,
 ) (*Publish, error) {
 	// 名前のバリデーション
 	if utf8.RuneCountInString(name) < nameLengthMin {
-		return nil, errDomain.NewError("name is invalid")
+		return nil, errDomain.NewError(fmt.Sprintf("出版社名は%d文字以上である必要があります", nameLengthMin))
 	}
 
 	// 名前(読み)のバリデーション
-	if utf8.RuneCountInString(namePhonic) < namePhonicLengthMin || !text.IsKatakana(namePhonic) {
-		return nil, errDomain.NewError("name is invalid")
+	if utf8.RuneCountInString(namePhonic) < namePhonicLengthMin {
+		return nil, errDomain.NewError(fmt.Sprintf("著者名読みは%d文字以上である必要があります", namePhonicLengthMin))
+	}
+
+	if !text.IsKatakana(namePhonic) {
+		return nil, errDomain.NewError("著者名読みはカタカナである必要があります")
 	}
 
 	// 日付のバリデーション(lastUpdateAtのほうが後か)
-	if createAt.After(lastUpdateAt) {
-		return nil, errDomain.NewError("createAt and lastUpdateAt are invalid")
+	if lastUpdateAt.Before(createAt) {
+		return nil, errDomain.NewError("更新日は作成日よりも後である必要があります")
 	}
 
+	// 削除フラグが立ってない もしくは 削除日は作成日よりも後であるか
+	if deletedAt != nil && deletedAt.Before(createAt) {
+		return nil, errDomain.NewError("削除日は作成日よりも後である必要があります")
+	}
 	return &Publish{
 		id:           id,
 		name:         name,
 		namePhonic:   namePhonic,
 		createAt:     createAt,
 		lastUpdateAt: lastUpdateAt,
+		deletedAt:    deletedAt,
 	}, nil
 }
 
@@ -59,8 +71,9 @@ func Reconstruct(
 	namePhonic string,
 	createAt time.Time,
 	lastUpdateAt time.Time,
+	deletedAt *time.Time,
 ) (*Publish, error) {
-	return newPublish(id, name, namePhonic, createAt, lastUpdateAt)
+	return newPublish(id, name, namePhonic, createAt, lastUpdateAt, deletedAt)
 }
 
 func NewPublish(
@@ -68,6 +81,7 @@ func NewPublish(
 	namePhonic string,
 	createAt time.Time,
 	lastUpdateAt time.Time,
+	deletedAt *time.Time,
 ) (*Publish, error) {
-	return newPublish(ulid.NewULID(), name, namePhonic, createAt, lastUpdateAt)
+	return newPublish(ulid.NewULID(), name, namePhonic, createAt, lastUpdateAt, deletedAt)
 }
